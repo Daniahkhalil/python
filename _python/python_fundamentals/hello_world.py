6aef5572216a88396dc6ebfa55434fa9a9d76186
Daniahkhalil <dhkhalil99@gmail.com>
2022-03-19 15:49:16 +0200

    print

---
+++ _python/python_fundamentals/hello_world.py
@@ -0,0 +1,14 @@
+print("Hello world !")
+
+name="Dania"
+print("Hello",name)
+print("Hello" +" "+name)
+
+num=5
+print("Hello",num)
+print("Hello"+ str(num))
+
+food1="pizza"
+food2="burgur"
+print("my first favorite food {0} my second favorite food {1}".format(food1,food2))
+print("my first favorite food %s my second favorite food %s" %(food1,food2))
