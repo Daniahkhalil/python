076ff375ad60f2c8cf24f867242bdb2b4aef4214
Daniahkhalil <dhkhalil99@gmail.com>
2022-03-22 13:26:23 +0200

    flask rendering

---
+++ flask/flask_fundamentals/templates/Understanding_Routing.py
@@ -0,0 +1,19 @@
+from flask import Flask  # Import Flask to allow us to create our app
+app = Flask(name)    # Create a new instance of the Flask class called "app"
+@app.route('/')          # The "@" decorator associates this route with the function immediately following
+def hello_world():
+    return 'Hello World!'  # Return the string 'Hello World!' as a response
+@app.route('/dojo')          # The "@" decorator associates this route with the function immediately following
+def dojo():
+    return 'have it say "Dojo!'  # Return the string 'Hello World!' as a response
+@app.route('/say/<name>')          # The "@" decorator associates this route with the function immediately following
+def say(name):
+    return 'Hi'+ name
+
+@app.route('/repeat/<num>/<name>')          # The "@" decorator associates this route with the function immediately following
+def repeat(num,name):
+    return name*int(num)
+
+
+if name=="main":
+    app.run(debug=True)
